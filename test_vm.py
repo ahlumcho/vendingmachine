3a33d30cea9ac911b67f7df187b9e18d8db4182d
ahlumcho <ahlum91@gmail.com>
2018-08-14 15:45:05 +0900

    알 수 없는 명령 처리

--- test_vm.py
+++ test_vm.py
@@ -4,10 +4,16 @@ def test_initial_change_should_be_zero() :
     assert "잔액은 0원입니다" == run("잔액")
 
 def test_insert_coin_and_check() :
-    assert "잔액은 0원입니다" == run("동전 100")
-    assert "100원을 넣었습니다" == run("잔액")
+    init()
+    assert "100원을 넣었습니다" == run("동전 100")
+    assert "잔액은 100원입니다" == run("잔액")
 
 def test_accumulation_of_change():
+    init()
     run("동전 100")
     run("동전 100")
-    assert "잔액은 200원입니다" == run("동전 100")
+    assert "잔액은 200원입니다" == run("잔액")
+
+def test_unknown_command():
+    init()
+    assert "알 수 없는 명령입니다" == run("웅앵")
--- vm.py
+++ vm.py
@@ -1,16 +1,20 @@
 change = 0
 
-def run(raw):
+def init():
     global change
     change = 0
+
+def run(raw):
+    global change
 
     tokens = raw.split(" ")
     cmd, params = tokens[0], tokens[1:]
 
-
     if cmd == "잔액":
         return "잔액은 " + str(change) + "원입니다"
-    else:
+    elif cmd == "동전":
         coin = params[0]
         change += int(params[0])
         return coin + "원을 넣었습니다"
+    else:
+        return "알 수 없는 명령입니다"
